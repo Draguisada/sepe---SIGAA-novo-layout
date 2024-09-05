476049b52b7ca09505a680d9f304d85428f62949
antonitsm <antonitsm30@gmail.com>
2024-09-05 20:23:47 -0300

    criado os templates dos cards de notas e funcionalidade de troca de cor

---
+++ Paginas/Principal/JS/code.js
@@ -0,0 +1,19 @@
+function colorizeNotas() {
+    const notaElements = document.querySelectorAll('.nota-conteudo-nota-valor');
+
+    notaElements.forEach(function(element) {
+        const notaValue = parseFloat(element.textContent);
+
+        if (notaValue < 6) {
+            element.style.backgroundColor = '#c8191e';
+        }
+        else if (notaValue >= 6 && notaValue < 7) {
+            element.style.backgroundColor = '#e9d20a';
+        }
+        else if (notaValue >= 7) {
+            element.style.backgroundColor = '#2f9e41';
+        }
+    });
+}
+
+colorizeNotas();
