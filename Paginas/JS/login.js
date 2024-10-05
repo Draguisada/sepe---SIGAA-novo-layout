5ef9dba72966227657f175eea5cead4fb9a64ffd
Draguisada <draguisada@gmail.com>
2024-10-04 22:07:48 -0300

    Login e Disciplinas Finalizado
    Adicionado interação com Logo IFC no Header para voltar ao Login
    
    Tela de Login com imagens de fundo randômicas

--- Paginas/JS/login.js
+++ Paginas/JS/login.js
@@ -1,3 +1,11 @@
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+
+
 function selectSection(elem) {
     let sections = document.getElementsByClassName('section');
 
@@ -11,4 +19,12 @@ function selectSection(elem) {
 function naoPossuiCadastro() {
     let hidden = document.getElementsByClassName('cadastro')
     hidden[0].classList.toggle('hidden')
+}
+
+let img_fundo = document.getElementsByClassName('img-fundo')
+
+
+
+for (i=0; i<img_fundo.length; i++) {
+    img_fundo[i].style.transform = "RotateZ(" + Math.ceil(Math.random() * 100) + "deg) Translate(" + getRandomIntInclusive(-25, 25) + "%)";
 }
